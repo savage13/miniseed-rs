bd1c048851553771941d6ddb1d9b3f36001f3090
Anthony Carapetis <anthony.carapetis@ga.gov.au>
2021-02-16 10:47:15 +1100

    Check out libmseed v2

--- build.rs
+++ build.rs
@@ -5,25 +5,26 @@ use std::env;
 use std::process::Command;
 use git2::Repository;
 
+const REPO_URL: &str = "https://github.com/iris-edu/libmseed";
+const BUILD_DIR: &str = "src/libmseed";
+const GIT_REF: &str = "refs/heads/2.x"; // libmseed v3 broke the ABI
 
 fn main() {
-    let url = "https://github.com/iris-edu/libmseed";
-    match Repository::clone(url, "src/libmseed") {
-        Ok(_repo) => {},
-        Err(e) => {
-            match e.code() {
-                git2::ErrorCode::Exists => {
-                    println!("directory exists");
-                },
-                _ => panic!("Failed to update/clone repo: {}", e),
-            }
+    let repo = match Repository::clone(REPO_URL, BUILD_DIR) {
+        Ok(_repo) => _repo,
+        Err(e) => match e.code() {
+            git2::ErrorCode::Exists =>
+                Repository::open(BUILD_DIR)
+                    .expect("Failed to open existing repo"),
+            _ => panic!("Failed to update/clone repo: {}", e),
         }
     };
+    let branch = repo.revparse_single(GIT_REF).unwrap();
+    repo.reset(&branch, git2::ResetType::Hard, None).unwrap();
 
-    let dir = "src/libmseed";
-    let path = std::fs::canonicalize(dir).unwrap();
-    let _ok = env::set_current_dir(&path).is_ok();
-    let _output = Command::new("make").output().expect("make failed");
+    let path = std::fs::canonicalize(BUILD_DIR).unwrap();
+    env::set_current_dir(&path).unwrap();
+    Command::new("make").output().expect("make failed");
 
     println!("cargo:rustc-link-search={}", path.to_str().unwrap());
     println!("cargo:rustc-link-lib={}", "mseed");
